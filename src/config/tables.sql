455ce955cc2b27bcfffdacb95f9da1b5fe2972b2
mahammad-rizwan <mahammadrizwan704@gmail.com>
2025-06-12 18:08:16 +0530

    conflict

--- src/config/tables.sql
+++ src/config/tables.sql
@@ -79,11 +79,41 @@ CREATE TABLE inspections (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE restaurants (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100),
+  license_number VARCHAR(50),
+  email VARCHAR(100),
+  phone VARCHAR(15),
+  zone VARCHAR(100),
+  region VARCHAR(100),
+  address TEXT,
+  status ENUM('pending', 'approved') DEFAULT 'pending',
+  created_by INT, -- inspector ID
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+ALTER TABLE restaurants
+ADD COLUMN last_inspection_date DATE DEFAULT NULL;
+
+
+
 INSERT INTO restaurants (id, name, license_number, email, phone, zone, region, address, status, created_by)
 VALUES
 (1, 'Spice Hub', 'LIC12345', 'spicehub@gmail.com', '9876543210', 'Bangalore South', 'Kengeri', '12, Kengeri Main Rd', 'approved', 101),
 (2, 'Green Leaf', 'LIC67890', 'greenleaf@gmail.com', '9123456789', 'Bangalore South', 'Pattanagere', '78, JP Nagar', 'approved', 102),
 (3, 'Tandoori Treat', 'LIC11122', 'treat@gmail.com', '9988776655', 'Bangalore Central', 'Indiranagar', '45, CMH Road', 'pending', 104),
 (4, 'Chennai Flavours', 'LIC33344', 'cf@gmail.com', '9080706050', 'Chennai North', 'Red Hills', '56, Red Hills Rd', 'approved', 106);
+
+
+INSERT INTO restaurants (id, name, license_number, email, phone, zone, region, address, status, created_by)
+VALUES
+(7, 'Spice Hub111', 'LIC12346', 'spicehub@gmail.com', '9876543210', 'Banglore South', 'Kengeri', '12, Kengeri Main Rd', 'approved', 101),
+(8, 'Spice Hub112', 'LIC12346', 'spicehub@gmail.com', '9876543210', 'Banglore South', 'Kengeri', '12, Kengeri Main Rd', 'approved', 101),
+(9, 'Spice Hub113', 'LIC12346', 'spicehub@gmail.com', '9876543210', 'Banglore South', 'Kengeri', '12, Kengeri Main Rd', 'approved', 101),
+(10, 'Spice Hub114', 'LIC12346', 'spicehub@gmail.com', '9876543210', 'Banglore South', 'Kengeri', '12, Kengeri Main Rd', 'approved', 101)
+;
+
+
 
 
