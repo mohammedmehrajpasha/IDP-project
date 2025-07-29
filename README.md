24607898b5483a3931688cfe1994e9311bb7583f
mohammed meraj <merajp786786@gmail.com>
2025-07-29 19:44:33 +0530

    Final

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ DB_NAME=''
 CLOUDINARY_CLOUD_NAME=''
 CLOUDINARY_API_KEY=''
 CLOUDINARY_API_SECRET=''
+
+Make sure you have the mysql set up with all the tables that are in the src/config/tables.sql
+
+To run the project execute the following commands in the terminal:
+1. npm i
+2. npm run dev
--- src/config/tables.sql
+++ src/config/tables.sql
@@ -22,28 +22,6 @@ CREATE TABLE admins (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO users (name, email, phone, password, assigned_region)
-VALUES 
-('Ananya Sharma', 'ananya.sharma@example.com', '9876543210', 'pass123', 'Delhi Central'),
-('Ravi Mehra', 'ravi.mehra@example.com', '9123456780', 'pass123', 'Bangalore North'),
-('Pooja Reddy', 'pooja.reddy@example.com', '9988776655', 'pass123', 'Hyderabad South');
-
-INSERT INTO admins (name, email, phone, password, zone)
-VALUES 
-('Meera Joshi', 'admin@gmail.com', '9876543210', 'pass123', 'Banglore South'),
-('Ravi Kumar', 'admin2@gmail.com', '9123456789', 'pass123', 'Banglore Central'),
-('Sneha Shah', 'admin3@gmail.com', '9012345678', 'pass123', 'Chennai North');
-
-INSERT INTO inspectors (name, email, phone, password, zone, region)
-VALUES 
-('Karthik Rao', 'insp@gmail.com', '7890123456', 'pass123', 'Banglore South', 'Kengeri'),
-('Anjali Menon', 'insp2@gmail.com', '7890654321', 'pass123', 'Banglore South', 'Pattanagere'),
-('Vikram Singh', 'insp3@gmail.com', '8989898989', 'pass123', 'Banglore South', 'Nayandahalli'),
-('Neha Verma', 'insp4@gmail.com', '9090909090', 'pass123', 'Banglore Central', 'Indiranagar'),
-('Sameer Patil', 'insp5@gmail.com', '8787878787', 'pass123', 'Banglore Central', 'Koramangala'),
-('Pooja Reddy', 'insp6@gmail.com', '8980077000', 'pass123', 'Chennai North', 'Red Hills'),
-('Aman Bhatt', 'insp7@gmail.com', '8567452301', 'pass123', 'Chennai North', 'Minjur');
-
 CREATE TABLE restaurants (
   id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(100),
@@ -61,18 +39,6 @@ CREATE TABLE restaurants (
   hygiene_score DECIMAL(2,1) CHECK (hygiene_score BETWEEN 1.0 AND 5.0),
   insp_rep_id INT
 );
-
-
-INSERT INTO restaurants (id, name, license_number, email, phone, zone, region, address, status, created_by)
-VALUES
-(1, 'Spice Hub', 'LIC12345', 'spicehub@gmail.com', '9876543210', 'Bangalore South', 'Kengeri', '12, Kengeri Main Rd', 'approved', 101),
-(2, 'Green Leaf', 'LIC67890', 'greenleaf@gmail.com', '9123456789', 'Bangalore South', 'Pattanagere', '78, JP Nagar', 'approved', 102),
-(3, 'Tandoori Treat', 'LIC11122', 'treat@gmail.com', '9988776655', 'Bangalore Central', 'Indiranagar', '45, CMH Road', 'pending', 104),
-(4, 'Chennai Flavours', 'LIC33344', 'cf@gmail.com', '9080706050', 'Chennai North', 'Red Hills', '56, Red Hills Rd', 'approved', 106),
-(7, 'Spice Hub111', 'LIC12346', 'spicehub@gmail.com', '9876543210', 'Banglore South', 'Kengeri', '12, Kengeri Main Rd', 'approved', 101),
-(8, 'Spice Hub112', 'LIC12346', 'spicehub@gmail.com', '9876543210', 'Banglore South', 'Kengeri', '12, Kengeri Main Rd', 'approved', 101),
-(9, 'Spice Hub113', 'LIC12346', 'spicehub@gmail.com', '9876543210', 'Banglore South', 'Kengeri', '12, Kengeri Main Rd', 'approved', 101),
-(10, 'Spice Hub114', 'LIC12346', 'spicehub@gmail.com', '9876543210', 'Banglore South', 'Kengeri', '12, Kengeri Main Rd', 'approved', 101);
 
 CREATE TABLE inspections (
   id INT NOT NULL AUTO_INCREMENT,
@@ -227,9 +193,4 @@ CREATE TABLE complaints (
   FOREIGN KEY (user_id) REFERENCES users(email) ON DELETE CASCADE,
   FOREIGN KEY (restaurant_id) REFERENCES restaurants(id) ON DELETE CASCADE
 );
-
-
-ALTER TABLE complaints ADD COLUMN images TEXT;
-
-ALTER TABLE complaints ADD COLUMN resolution_taken TEXT AFTER message;
 
